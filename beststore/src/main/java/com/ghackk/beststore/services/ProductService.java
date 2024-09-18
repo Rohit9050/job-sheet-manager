d06370b6bdd9759fed6cc7b74cca6fecc69e06d2
Rohit9050 <131391879+Rohit9050@users.noreply.github.com>
2024-09-18 20:40:03 +0530

    Update ProductService.java
    Final Commit

--- beststore/src/main/java/com/ghackk/beststore/services/ProductService.java
+++ beststore/src/main/java/com/ghackk/beststore/services/ProductService.java
@@ -4,14 +4,14 @@ import com.ghackk.beststore.models.Product;
 import java.util.List;
 
 public interface ProductService {
-	
+	// Get All Products
     List<Product> getAllProducts();
-    
+    //Get Prdouct By Id
     Product getProductById(int id);
-    
+    //Save Product
     void saveProduct(Product product);
-    
+    //Delete Product
     void deleteProductById(int id);
-    
+    //Search Product with name
     List<Product> searchProducts(String keyword);
 }
